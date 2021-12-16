2deb17417cd8b368daf401876842afb860919c97
Danilo Souza <danilo_souza19@outlook.com>
2021-12-16 16:22:15 -0300

    add test to ShowUserProfileUseCase

---
+++ src/modules/users/useCases/showUserProfile/ShowUserProfileUseCase.spec.ts
@@ -0,0 +1,26 @@
+import { InMemoryUsersRepository } from "../../repositories/in-memory/InMemoryUsersRepository";
+import { ShowUserProfileError } from "./ShowUserProfileError";
+import { ShowUserProfileUseCase } from "./ShowUserProfileUseCase";
+
+let showUserProfileUseCase: ShowUserProfileUseCase;
+let usersRepositoryInMemory: InMemoryUsersRepository;
+
+describe('Show the user profile', () => {
+  beforeEach(() => {
+    usersRepositoryInMemory = new InMemoryUsersRepository();
+
+    showUserProfileUseCase = new ShowUserProfileUseCase(usersRepositoryInMemory);
+  });
+
+  it('Should be able to create a new User', async () => {
+    const user = await usersRepositoryInMemory.create({
+      email: 'johndoe@example.com',
+      name: 'John Doe',
+      password: 'some-password'
+    });
+
+    const userProfile = await showUserProfileUseCase.execute(user.id as string);
+
+    expect(userProfile).toEqual(user);
+  });
+});
