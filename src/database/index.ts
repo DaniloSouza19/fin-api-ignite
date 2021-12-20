a5041ba328df24dc7a6926bdf67bc9da4bb14f49
Danilo Souza <danilo_souza19@outlook.com>
2021-12-20 15:13:36 -0300

    add conditional database (test)

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,3 +1,18 @@
-import { createConnection } from 'typeorm';
+import {
+  Connection,
+  createConnection as createConnectionTypeOrm,
+  getConnectionOptions
+} from 'typeorm';
 
-(async () => await createConnection())();
+async function createConnection(): Promise<Connection> {
+  const options = await getConnectionOptions();
+
+  const newOptions = Object.assign(options, {
+    database: process.env.NODE_ENV === 'test'
+    ? process.env.DATABASE_TEST : options.database
+  });
+
+  return createConnectionTypeOrm(newOptions);
+}
+
+export { createConnection }
